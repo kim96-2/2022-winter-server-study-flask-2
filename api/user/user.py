c8bc6d71d4797bc2a82abb93b137a910f5c1dec0
LEEJH <totolikid@naver.com>
2023-02-06 01:10:25 +0900

    Error All Fixed

--- api/user/user.py
+++ api/user/user.py
@@ -8,19 +8,20 @@ user = Namespace('user')
 class UserManagement(Resource):
     def get(self):
         # GET method 구현 부분
+        db =Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABase.user where id = %s;"
-        row = Database.execute_one(self,quary,(userData['id']))
+        quary = "select * from JaeHyukDATABASE.user where id = \"%s\";"
+        row = db.execute_one(quary,(userData['id']))
 
-        if row['id'] is None:
-            response = jsonify({"is_success":False ,"message" : "아이디어나 비밀번호 불일치"})
+        if row is None:
+            response = jsonify({"is_success":False ,"message" : "해당 유저 존재하지 않음"})
             return make_response(response,400)
 
-        quary = "select * from JaeHyukDATABase.user where id = %s and pw = %s;"
-        row = Database.execute_one(self,quary,(userData['id'],userData['pw']))
+        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
+        row = db.execute_one(quary,(userData['id'],userData['passward']))
 
-        if row['id'] is None:
+        if row is None:
             response = jsonify({"is_success":False ,"message" : "아이디어나 비밀번호 불일치"})
             return make_response(response,400)
 
@@ -29,18 +30,19 @@ class UserManagement(Resource):
 
     def post(self):
         # POST method 구현 부분
+        db =Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABase.user where id = %s;"
-        row = Database.execute_one(self,quary,(userData['id']))
+        quary = "select * from JaeHyukDATABASE.user where id = \"%s\";"
+        row = db.execute_one(quary,(userData['id']))
 
-        if row['id'] is not None:
+        if row is not None:
             response = jsonify({"is_success":False ,"message" : "이미 있는 유저"})
             return make_response(response,400)
              
-        quary = "insert into JaeHyukDATABase.user values(%s,%s,%s);"
-        Database.execute(self,quary,(userData['id'],userData['passward'],userData['nickname']))
-        Database.commit(self)
+        quary = "insert into JaeHyukDATABASE.user values(\"%s\",\"%s\",\"%s\");"
+        db.execute(quary,(userData['id'],userData['passward'],userData['nickname']))
+        db.commit()
 
         response = jsonify({"is_success":True ,"message" : "유저 생성 성공"})
         return make_response(response,200)
@@ -48,26 +50,28 @@ class UserManagement(Resource):
 
     def put(self):
         # PUT method 구현 부분
+        db =Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABase.user where id = %s and pw = %s;"
-        row = Database.execute_one(self,quary,(userData['id'],userData['pw']))
+        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
+        row = db.execute_one(quary,(userData['id'],userData['passward']))
 
-        if row['id'] is None:
+        if row is None:
             response = jsonify({"is_success":False ,"message" : "아이디어나 비밀번호 불일치"})
             return make_response(response,400)
 
-        if row['nickname'] is userData['nickname']:
+        #return row['nickname'] +" and "+ userData['nickname']
+        if row['nickname'] == "'"+userData['nickname']+"'":
             response = jsonify({"is_success":False ,"message" : "현재 닉네임과 같음"})
             return make_response(response,400)
 
         quary = "Set SQL_SAFE_UPDATES = 0;"
-        Database.execute(self,quary)
-        Database.commit(self)
+        db.execute(quary)
+        db.commit()
 
-        quary = "update JaeHyukDATABase.user set nickname = %s where nickname = %s;"
-        Database.execute(self,quary,(userData['nickname'],row['nickname']))
-        Database.commit(self)
+        quary = "update JaeHyukDATABASE.user set nickname = \"%s\" where nickname = \"%s\";"
+        db.execute(quary,(userData['nickname'],row['nickname']))
+        db.commit()
 
         response = jsonify({"is_success":True ,"message" : "유저 닉네임 변경 성공"})
         return make_response(response,200)
@@ -75,18 +79,19 @@ class UserManagement(Resource):
 
     def delete(self):
         # DELETE method 구현 부분
+        db = Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABase.user where id = %s and pw = %s;"
-        row = Database.execute_one(self,quary,(userData['id'],userData['pw']))
+        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
+        row = db.execute_one(quary,(userData['id'],userData['passward']))
 
-        if row['id'] is None:
+        if row is None:
             response = jsonify({"is_success":False ,"message" : "아이디어나 비밀번호 불일치"})
             return make_response(response,400)
 
-        quary = "Delete from JaeHyukDATABase.user where id = %s;"
-        Database.execute(self,quary,(userData['id']))
-        Database.commit(self)
+        quary = "Delete from JaeHyukDATABASE.user where id = \"%s\";"
+        db.execute(quary,(userData['id']))
+        db.commit()
 
         response = jsonify({"is_success":True ,"message" : "유저 삭제 성공"})
         return make_response(response,200)
