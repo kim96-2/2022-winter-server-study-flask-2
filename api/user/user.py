c254bdac5ddae6a5a32ae131101bc24defa95b5b
LEEJH <totolikid@naver.com>
2023-02-08 01:36:32 +0900

    commit

--- api/user/user.py
+++ api/user/user.py
@@ -9,17 +9,19 @@ class UserManagement(Resource):
     def get(self):
         # GET method 구현 부분
         db =Database()
-        userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABASE.user where id = \"%s\";"
-        row = db.execute_one(quary,(userData['id']))
+        id = request.args.get('id')
+        pw = request.args.get('password')
+
+        quary = "select * from JaeHyukDATABASE.user where id = %s;"
+        row = db.execute_one(quary,(id))
 
         if row is None:
             response = jsonify({"is_success":False ,"message" : "해당 유저가 존재하지 않음"})
             return make_response(response,400)
 
-        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
-        row = db.execute_one(quary,(userData['id'],userData['password']))
+        quary = "select * from JaeHyukDATABASE.user where id = %s and pw = %s;"
+        row = db.execute_one(quary,(id,pw))
 
         if row is None:
             response = jsonify({"is_success":False ,"message" : "아이디나 비밀번호 불일치"})
@@ -33,14 +35,14 @@ class UserManagement(Resource):
         db =Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABASE.user where id = \"%s\";"
+        quary = "select * from JaeHyukDATABASE.user where id = %s;"
         row = db.execute_one(quary,(userData['id']))
 
         if row is not None:
             response = jsonify({"is_success":False ,"message" : "이미 있는 유저"})
             return make_response(response,400)
              
-        quary = "insert into JaeHyukDATABASE.user values(\"%s\",\"%s\",\"%s\");"
+        quary = "insert into JaeHyukDATABASE.user values(%s,%s,%s);"
         db.execute(quary,(userData['id'],userData['password'],userData['nickname']))
         db.commit()
 
@@ -53,7 +55,7 @@ class UserManagement(Resource):
         db =Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
+        quary = "select * from JaeHyukDATABASE.user where id = %s and pw = %s;"
         row = db.execute_one(quary,(userData['id'],userData['password']))
 
         if row is None:
@@ -61,15 +63,11 @@ class UserManagement(Resource):
             return make_response(response,400)
 
         #return row['nickname'] +" and "+ userData['nickname']
-        if row['nickname'] == "'"+userData['nickname']+"'":
+        if row['nickname'] == userData['nickname']:
             response = jsonify({"is_success":False ,"message" : "현재 닉네임과 같음"})
             return make_response(response,400)
 
-        quary = "Set SQL_SAFE_UPDATES = 0;"
-        db.execute(quary)
-        db.commit()
-
-        quary = "update JaeHyukDATABASE.user set nickname = \"%s\" where nickname = \"%s\";"
+        quary = "update JaeHyukDATABASE.user set nickname = %s where nickname = %s;"
         db.execute(quary,(userData['nickname'],row['nickname']))
         db.commit()
 
@@ -82,14 +80,14 @@ class UserManagement(Resource):
         db = Database()
         userData = request.get_json()
 
-        quary = "select * from JaeHyukDATABASE.user where id = \"%s\" and pw = \"%s\";"
+        quary = "select * from JaeHyukDATABASE.user where id = %s and pw = %s;"
         row = db.execute_one(quary,(userData['id'],userData['password']))
 
         if row is None:
             response = jsonify({"is_success":False ,"message" : "아이디나 비밀번호 불일치"})
             return make_response(response,400)
 
-        quary = "Delete from JaeHyukDATABASE.user where id = \"%s\";"
+        quary = "Delete from JaeHyukDATABASE.user where id = %s;"
         db.execute(quary,(userData['id']))
         db.commit()
 
